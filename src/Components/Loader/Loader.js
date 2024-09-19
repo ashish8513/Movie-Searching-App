957882aa5b70e8a90ef3244663db0dc54d6e05fd
Ashish Prabhakar <ashishprabhakar1010@gmail.com>
2024-09-19 11:02:14 +0530

    changes

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -9,7 +9,7 @@ export default function Loading() {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 100 100"
           fill="none"
-          stroke="#4F46E5"
+          stroke="#121212"
           className="spinner-svg"
         >
           <circle
